b7d6b66c585940af426765ee34d76ba185458e3c
Jack de Grave <jack.degrave@outlook.com>
2019-05-27 18:34:05 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,6 +2,7 @@ def speak_to_grandma(phrase)
   if "#{phrase}" == phrase.upcase
     return "NO, NOT SINCE 1938!"
   else if "#{phrase}" == "I LOVE YOU GRANDMA!"
+    return ""
   else
     return "HUH?! SPEAK UP, SONNY!"
   end
